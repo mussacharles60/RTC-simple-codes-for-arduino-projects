94a482c60eb2b3609b0fd19f081eae70cc949250
Mussa Charles <mussacharles60@gmail.com>
2019-05-17 01:35:09 +0300

    Add files via upload

---
+++ Run_clock.ino
@@ -0,0 +1,86 @@
+/* This is the simple code for read and run the clock from every RTC module with i2c but protocal...
+
+Created by Mussa Charles (Myssa)
+
+Visit http://www.github.com/mussacharles60 for more info.
+
+code no. 2 (run clock).
+*/
+
+#include <Wire.h>
+
+uint8_t second, minute, hour, wday, day, month, year, ctrl;
+
+//this is a list of long variables used in this clock program
+long s;
+long m;
+long h;
+long sec;
+long minutes;
+long hrs;
+
+void setup() {
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect
+  }
+  Wire.begin();
+}
+
+void loop() {
+  if (read_ds1307()) {
+    hours();
+    mins();
+    secs();
+      
+    Serial.print("Date: ");
+    Serial.print(day);
+    Serial.print('/');
+    Serial.print(month);
+    Serial.print('/');
+    Serial.print(year);
+    Serial.print("  Clock: ");
+    Serial.print(hour);
+    Serial.print(':');
+    Serial.print(minute);
+    Serial.print(':');
+    Serial.print(second);
+    Serial.print(" ");
+    Serial.print(wday);
+    Serial.println();
+    delay(1000);
+  
+  }
+  else {
+    Serial.println("Error!!... DS1307 not detected");
+  }
+  
+}
+
+bool read_ds1307() {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x00);
+  if (Wire.endTransmission() != 0);
+    return false; 
+  Wire.requestFrom(0x68, 8);
+  second = bcd2bin(Wire.read());
+  minute = bcd2bin(Wire.read());
+  hour = bcd2bin(Wire.read());
+  wday = bcd2bin(Wire.read());
+  day = bcd2bin(Wire.read());
+  month = bcd2bin(Wire.read());
+  year = bcd2bin(Wire.read());
+
+  ctrl = Wire.read();
+  return true;                 
+}
+
+uint8_t bcd2bin(uint8_t bcd) {
+  return (bcd / 16 * 10) + (bcd % 16);
+}
+
+long hours()   { hrs =  hour; h =  hour; return hrs;           } //this method is for hours
+long mins()    { minutes = minute; m = minute; return minutes; } //this method is for minutes
+long seconds() { s = second; return s;                         } //this method is for seconds
+long secs()    { sec = second; return sec;                     }
+
---
+++ Set_clock.ino
@@ -0,0 +1,73 @@
+/* This is the simple code for setting the clock for almost every RTC module which uses the i2c bus protocal...
+
+created by Mussa Charles (Myssa)
+
+Visit http://www.github.com/mussacharles60 for more info.
+
+code no. 1 (set clock).
+*/
+
+#include <Wire.h>
+
+uint8_t second, minute, hour, wday, day, month, year;
+
+voidid setup() {
+   Serial.begin(9600);
+   Wire.begin();
+
+// Enter new date & clock below
+   second = 00;
+   minute = 15;
+   hour = 21;
+   wday = 3;
+   day = 19;
+   month = 03;
+   year = 19;
+}
+
+void loop() {
+   write_ds1307();
+
+   Serial.println("Setting date & clock to DS1307 Tiny RTC:");
+   print_time();
+
+   for (;;);
+}
+bool write_ds1307() {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x00);
+  Wire.write(bin2bcd(second & 0x7F));
+  Wire.write(bin2bcd(minute));
+  Wire.write(bin2bcd(hour));
+  Wire.write(bin2bcd(wday));
+  Wire.write(bin2bcd(day));
+  Wire.write(bin2bcd(month));
+  Wire.write(bin2bcd(year));
+
+  if (Wire.endTransmission() != 0)
+    return false;
+
+  return true;
+}
+
+uint8_t bin2bcd(uint8_t bin) {
+  return (bin / 10 * 16) + (bin % 10);
+}
+
+void print_time() {
+  Serial.print("Date: ");
+  Serial.print(day);
+  Serial.print('/');
+  Serial.print(month);
+  Serial.print('/');
+  Serial.print(year);
+  Serial.print("  Clock: ");
+  Serial.print(hour);
+  Serial.print(':');
+  Serial.print(minute);
+  Serial.print(':');
+  Serial.print(second);
+  Serial.print(" ");
+  Serial.print(wday);
+  Serial.println();
+}
